e55ded9a614b0267858787dc4bb4e853a76a4ce3
Shema Jolivet Gislain <shemajolivet@gmail.com>
2024-08-03 13:19:46 +0200

    Formatted  project readme

--- src/pages/Home/child/AddComment.jsx
+++ src/pages/Home/child/AddComment.jsx
@@ -1,12 +1,40 @@
 import { Controller, useForm } from "react-hook-form";
 import TextArea from "../../../components/Inputs/TextArea";
 import Button from "../../../components/Button";
+import PropTypes from "prop-types";
+import { useAddBlogCommentMutation } from "../../../app/api";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 const AddComment = ({ post_id }) => {
   const { control, handleSubmit } = useForm();
+
+  const [addBlogComment, { isLoading, isSuccess, isError }] =
+    useAddBlogCommentMutation();
   const onSubmit = (data) => {
     console.log(data);
+    addBlogComment({ id: post_id, data });
   };
+
+  //   Handling comments Submittion
+  useEffect(() => {
+    if (!isLoading && isSuccess) {
+      toast.success("Comment added successfully", {
+        autoClose: 2000,
+        hideProgressBar: true,
+        position: "top-center",
+      });
+
+      // Reset form
+      control.reset({ content: "" });
+    } else if (isError) {
+      toast.error("Error Occured while adding comment", {
+        autoClose: 2000,
+        hideProgressBar: true,
+        position: "top-center",
+      });
+    }
+  }, [control, isError, isLoading, isSuccess]);
   return (
     <div className="w-1/2 flex flex-col gap-3">
       <header>
@@ -41,6 +69,10 @@ const AddComment = ({ post_id }) => {
       </form>
     </div>
   );
+};
+
+AddComment.propTypes = {
+  post_id: PropTypes.string,
 };
 
 export default AddComment;
